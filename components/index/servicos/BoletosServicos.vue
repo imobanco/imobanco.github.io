0c43cfb12c5e3cb9fdf81dd4098a50bd83277ca6
phelipequeiroz <phelipedaniel84@gmail.com>
2020-04-26 21:09:24 -0300

    boleto index detalhe

--- components/index/servicos/BoletosServicos.vue
+++ components/index/servicos/BoletosServicos.vue
@@ -3,7 +3,6 @@
         <div class="container servicoestiloleft">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-10 pt-5 pb-5">
-                    
                     <titulo-servicos
                     class="text-white"
                     icon="description" 
@@ -12,33 +11,50 @@
 
                     <div class="row">
                         <div class="col-lg-6">
-                            <p class="text-white text-justify"><span class="text-success"><i class="material-icons" style="font-size: 13px !important;">lens</i></span> Emita boletos e envie por e-mail gratuitamente. Nosso sistema enviará automaticamente os lembretes de vencimento para seus clientes.</p>
+                        <texto-detalhe
+                        icon="lens"
+                        texto="Emita boletos recorrentes e envie por e-mail gratuitamente." />
                         </div>
+
                         <div class="col-lg-6">
-                            <p class="text-white text-justify"><span class="text-success"><i class="material-icons" style="font-size: 13px !important;">lens</i></span> Acompanhe o seu cliente - Visualize quando o seu cliente abre os e-mails e faturas enviadas por você.</p>
+                        <texto-detalhe
+                        icon="lens"
+                        texto="Acompanhe seu cliente - Visualize quando os e-mails são abertos" />
                         </div>
+
                         <div class="col-lg-6">
-                            <p class="text-white text-justify"><span class="text-success"><i class="material-icons" style="font-size: 13px !important;">lens</i></span> Seja cobrado apenas pelas cobranças que forem pagas</p>
+                        <texto-detalhe
+                        icon="lens"
+                        texto="Nosso sistema enviará os lembretes de vencimento para seus clientes." />
                         </div>
+                        
                         <div class="col-lg-6">
-                            <h5 class="text-titulo"><span class="text-warning"><i class="material-icons" style="font-size: 20px !important;">local_offer</i></span> <strong class="text-warning">Recurso exclusivo Imobanco: </strong></h5>
-                            <p class="text-white text-justify"> Cadastre seu boleto individual ou contrato de parcelas a receber, juntamente com o plano de divisão de valor, se for necessário. Com a liquidação do boleto, nosso sistema já transfere os valores planejados imediatamente.</p>
+                        <texto-detalhe
+                        icon="lens"
+                        texto="Pague somente os titulos recuperados." />
                         </div>
+
+                    </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <img src="~/assets/image/muckup-01.png" alt="" class="img-fluid">
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <img src="~/assets/image/muckup-01.png" alt="" class="img-fluid">
+
+                
                 </div>
-            </div>
-        </div>
+                
     </section>
 </template>
 
 <script>
    import TituloServicos from '~/components/index/servicos/TituloServicos.vue'
+   import TextoDetalhe from '~/components/index/servicos/TextoDetalhe.vue'
     export default {
         components: {
-        TituloServicos
+        TituloServicos,
+        TextoDetalhe
   }
     }
 </script>
---
+++ components/index/servicos/TextoDetalhe.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+            <p class="text-white"><span class="text-success"><i class="material-icons" style="font-size: 13px !important;">{{icon}}</i></span> {{texto}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TextoDetalhe',
+        props: ['texto', 'icon'],
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
