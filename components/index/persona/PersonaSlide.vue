2105cb04008ae7b543a2e3936d47be47ee9881fb
Phelipe <phelipedaniel84@gmail.com>
2020-08-04 02:13:58 -0300

    section personas em desenvolvimento

---
+++ components/index/persona/PersonaSlide.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <img v-for="(media, index) in galery_picture" :src="media.picture" alt class="img-fluid" />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "PersonaSlide",
+  data() {
+    return {
+      galery_picture: [
+        {
+          picture: "image/persona-01.jpg",
+        },
+        {
+          picture: "image/persona-02.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
