b8fc41b90396bdbc8c781c66e70862dfdaa4ca70
Phelipe <phelipedaniel84@gmail.com>
2020-08-06 23:23:27 -0300

    Seção index - Pensado e desenvolvido para você!

--- components/index/persona/PersonaSlide.vue
+++ components/index/persona/PersonaSlide.vue
@@ -1,45 +1,10 @@
 <template>
   <b-container fluid class="bg-gray-500">
     <b-row>
-      <b-col lg="12" class="text-center">
-        <img v-for="(media, index) in galery_picture" :src="media.picture" alt :class="fluid" />
+      <b-col lg="12" class="text-center bg-primary">
+        <img src="/image/persona-01.jpg" alt="sequencia imagem" class="img-fluid" />
       </b-col>
     </b-row>
   </b-container>
 </template>
 
-<script>
-export default {
-  name: "PersonaSlide",
-  data() {
-    return {
-      galery_picture: [
-        {
-          picture: "image/persona-01.jpg",
-          fluid: "img-fluid",
-        },
-        {
-          picture: "image/persona-02.jpg",
-          fluid: "img-fluid",
-        },
-        {
-          picture: "image/persona-03.jpg",
-          fluid: "img-fluid",
-        },
-        {
-          picture: "image/persona-04.jpg",
-          fluid: "img-fluid",
-        },
-
-        // {
-        //   picture: "image/persona-06.jpg",
-        //   fluid: "img-fluid",
-        // },
-      ],
-    };
-  },
-};
-</script>
-
-<style lang="scss" scoped>
-</style>
