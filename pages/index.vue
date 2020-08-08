9df9cfe4902822c360dc64c8474d528534dde678
Phelipe <phelipedaniel84@gmail.com>
2020-08-08 19:55:29 -0300

    section formas imobanco

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <header-slide />
     <persona-section />
+    <formas-item />
   </div>
 </template>
 
 <script>
 import HeaderSlide from "~/components/main/HeaderSlide.vue";
 import PersonaSection from "~/components/index/persona/PersonaSection.vue";
+import FormasItem from "~/components/index/formasimobanco/FormasItem.vue";
 
 export default {
   components: {
     HeaderSlide,
     PersonaSection,
+    FormasItem,
   },
 };
 </script>
